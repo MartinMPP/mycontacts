d2c9db3a34372c1dffbe0256d7ab93f5380dcfdc
Martin P Ponce <martinpp.particular@gmail.com>
2024-10-10 15:44:22 -0300

    Criei um portal para o modal e vou criar um Loader agora

--- fe/src/components/Button.js
+++ fe/src/components/Button.js
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 
 export default styled.button`
-    width: 100%;
+    padding: 0 16px;
     height: 52px;
     border: none;
     background: ${({ theme }) => theme.colors.primary.main};
@@ -23,5 +23,15 @@ export default styled.button`
         background: #ccc;
         cursor: default;
     }
+
+    ${({ theme, danger }) => danger && css`
+        background: ${theme.colors.danger.main};
+        &:hover{
+        background: ${theme.colors.danger.light};
+    }
+        &:active{
+        background: ${theme.colors.danger.dark};
+    }
+    `}
 
 `;
